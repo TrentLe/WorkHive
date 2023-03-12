39d43f305832ebafac5a2eb9776d1652a2150f6e
unknown <raeburn.trent6@gmail.com>
2023-03-12 15:11:22 -0400

    added following list

---
+++ client/src/components/Following/index.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_FOLLOW } from '../../utils/mutations';
+import { QUERY_FOLLOWEDUSER } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
+const FollowForm = () => {
+    const [followedUser, setFollowedUser] = useState([]);
+    const [followedUserCount, setFollowedUserCount] = useState(0);
+
+const [addFollow, { error }] = useMutation(ADD_FOLLOW, {
+    update(cache, { data: { addFollow } }) {
+        try {
+            const { followedUser } = cache.readQuery({ query: QUERY_FOLLOWEDUSER });
+            cache.writeQuery({
+                query: QUERY_FOLLOWEDUSER,
+                data: { followedUser: [addFollow, ...followedUser] },
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    },
+});
+
+const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+        const { data } = await addFollow({
+            variables: {
+                followedUser,
+                followedUserCount,
+            },
+        });
+
+        setFollowedUser('')
+        setFollowedUserCount(0);
+    } catch (err) {
+        console.error(err);
+    }
+
+    window.location.reload();
+}
+
+const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'followedUser' && value.length <= 280) {
+        setFollowedUser(value);
+        setFollowedUserCount(value.length);
+    }
+}
+
+return (
+    <div>
+        <h1>User</h1>
+        <p>Followed User: {followedUser}</p>
+        <p>Followed User Count: {followedUserCount}</p>
+        <form onSubmit={handleFormSubmit}>
+            <div>
+                <label htmlFor="followedUser">Followed User:</label>
+                <input
+                    placeholder="Followed User"
+                    name="followedUser"
+                    type="text"
+                    id="followedUser"
+                    value={followedUser}
+                    onChange={handleChange}
+                />
+            </div>
+            </form>
+            </div>
+
+
+);
+
+}
+
+
+
+
+
+export default FollowForm;
