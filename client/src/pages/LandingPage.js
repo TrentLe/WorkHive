2784c56df1f0c29de27c227d150c85744b061085
TrentLe <raeburn.trent6@gmail.com>
2023-03-15 13:59:40 -0400

    Profile

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Login from "../components/Login/Login";
+import Signup from "../components/Signup/Signup";
+
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+import {motion} from 'framer-motion'
+
+
+
+const Section = styled.section`
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:black;
+
+`;
+
+const Container = styled.div`
+display:grid;
+grid-template-column: 1fr 1fr;
+height: 100vh;
+padding: 3rem calc ((100vwv - 1300px)/2);
+
+@media screen and (max-width:768px) {
+  grid-grid-template-columns: 1fr;
+}`;
+
+
+const LandingPage = () => {
+
+  
+    return (
+
+      <section>
+        <container className="container">
+          <Link to='/companylanding'><button>Signup or Login as a Company</button></Link>
+
+          <Login/>        
+          <Signup/>
+        </container>
+      </section>
+    );
+  };
+  
+  export default LandingPage;
--- client/src/pages/Meetup.js
+++ client/src/pages/Meetup.js
@@ -17,6 +17,15 @@ function Meetup() {
             opportunities, network with industry professionals, and take the
             next step in your career.{" "}
           </p>
+<h1 style={{marginTop: "7%"}}>Sign up for our Jobs Newsletter today!</h1>
+<button href="/Contact">Sign Up</button>
+
+          <img
+            src="https://cdn.vectorstock.com/i/1000x1000/20/51/job-proposal-abstract-concept-vector-33672051.webp"
+            alt="On the spot interview"
+            style={{ maxWidth: "100%", height: "auto", 
+            clipPath: "inset(0px 0px 85px 0px)", marginTop: "2%" }}
+          />
         </div>
         <div style={{ flex: 1 }}>
           <iframe
